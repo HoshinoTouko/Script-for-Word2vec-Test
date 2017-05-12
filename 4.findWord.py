2b41819d8b199faaacb267cdb5049c61433d0def
Touko <HoshinoTouko@users.noreply.github.com>
2017-05-12 15:30:34 +0800

    Create 4.findWord.py

---
+++ 4.findWord.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+
+import word2vec
+import sysprint
+
+word = sys.argv[0]
+model = word2vec.load('corpusWord2Vec.bin')
+indexes = model.cosine(word.decode)
+for index in indexes[0]:
+    print (model.vocab[index])
