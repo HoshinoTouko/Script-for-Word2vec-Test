bbe78b2f9c167ce93c13705565b99c0ebff95258
HoshinoTouko <root@vultr.guest>
2017-05-12 08:15:49 +0000

    fing word func sucess

--- 4.findWord.py
+++ 4.findWord.py
@@ -3,8 +3,8 @@
 import word2vec
 import sys
 
-# word = sys.argv[0]
+word = sys.argv[1]
 model = word2vec.load('corpusWord2Vec.bin')
-indexes = model.cosine(u'橙子')
+indexes = model.cosine(word.decode('utf-8'))
 for index in indexes[0]:
     print (model.vocab[index])
